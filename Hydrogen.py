8ecf148f65cc456a4ff8f3b5b8b53fcb7e2c0434
Volalex2000 <volkov.ad@phystech.edu>
2024-11-12 17:52:08 +0100

    1.2 version of Hydrogen.py

--- Hydrogen.py
+++ Hydrogen.py
@@ -17,6 +17,9 @@ class Hydrogen:
 
         Parameters:
         n (int): The principal quantum number.
+
+        Returns:
+        float: The energy of the Hydrogen atom for the given principal quantum number.
         """
         return -13.6 * self.q**2 / n**2 # eV
     
@@ -26,7 +29,10 @@ class Hydrogen:
         The potential is given in electron volts (eV).
 
         Parameters:
-        x (float): The distance |x| from the nucleus.
+        x (np.array): The distance |x| from the nucleus.
+
+        Returns:
+        np.array: The value of the potential at the given distance.
         """
         return -self.q / (np.abs(x) + 1e-10) # eV
     
@@ -36,6 +42,30 @@ class Hydrogen:
         The potential is given in electron volts (eV).
 
         Parameters:
-        x (float): The distance |x| from the nucleus.
+        x (np.array): The distance |x| from the nucleus.
+
+        Returns:
+        np.array: The value of the potential at the given distance.
         """
         return -self.q / np.sqrt(2 + x**2) # eV
+    
+    def ground_state_energy(self):
+        """
+        Calculate the ground state energy of the Hydrogen atom.
+        The energy is given in electron volts (eV).
+        """
+        return self.energy(1)
+    
+    def ground_state_wavefunction(self, x):
+        """
+        Calculate the ground state wavefunction of the Hydrogen atom at a distance |x| from the nucleus.
+        The wavefunction is given in arbitrary units.
+
+        Parameters:
+        x (np.array): The distance |x| from the nucleus.
+
+        Returns:
+        np.array: The value of the wavefunction at the given distance.
+        """
+        a = 0.529 / self.q # Bohr radius in angstrom
+        return 1/np.sqrt(a) * np.exp(-np.abs(x) / a)
