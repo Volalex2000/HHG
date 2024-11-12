7ca9db37f19a966fc25e7651484d7fc601d72c86
Volalex2000 <volkov.ad@phystech.edu>
2024-11-12 17:34:46 +0100

    1.0 version of Parameters.py

---
+++ Parameters.py
@@ -0,0 +1,23 @@
+def pars_YanPengPhysRevA_78_033821():
+    return [
+        [ 
+            5, # Durations, fs 
+            3.5e14, # Amplitudes, W/cm^2
+            0.057, # Frequencies, a. u.
+            0 # Phases
+        ],
+
+        [
+            [5, 5, 5], # Durations, fs 
+            [3.0e14, 1.0e14, 2.0e14], # Amplitudes, W/cm^2
+            [0.057, 0.114, 0.171], # Frequencies, a. u.
+            0 # Phases ???
+        ],
+
+        [
+            [5, 5], # Durations, fs 
+            [3.2e14, 1.1e14], # Amplitudes, W/cm^2
+            [0.057, 0.114], # Frequencies, a. u.
+            0 # Phases ???
+        ]
+    ]
