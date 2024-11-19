27c01a89698775c61cd2c2294660df627480b863
Volalex2000 <volkov.ad@phystech.edu>
2024-11-19 14:24:55 +0100

    1.2 version of Parameters.py

--- Parameters.py
+++ Parameters.py
@@ -1,5 +1,5 @@
 def pars_YanPengPhysRevA_78_033821():
-    return [
+    return np.array([
         [ 
             206.8, # Durations, a. u.
             0.00997, # Amplitudes, a. u. 
@@ -11,13 +11,27 @@ def pars_YanPengPhysRevA_78_033821():
             [206.8, 206.8, 206.8], # Durations, a. u. 
             [0.00855, 0.00285, 0.00570], # Amplitudes, a. u.
             [0.057, 0.114, 0.171], # Frequencies, a. u.
-            [0, 0, 0] # Phases ???
+            [0, 0, 0] # Phases, rad
         ],
 
         [
             [206.8, 206.8], # Durations, a. u.
             [0.00912, 0.00313], # Amplitudes, a. u.
             [0.057, 0.114], # Frequencies, a. u.
-            [0, 0, 0] # Phases ???
+            [0, 0] # Phases, rad
+        ],
+
+        [
+            [165.4, 165.4, 165.4], # Durations, a. u. 
+            [0.00855, 0.00285, 0.00570], # Amplitudes, a. u.
+            [0.057, 0.114, 0.171], # Frequencies, a. u.
+            [0, 0, 0] # Phases, rad
+        ],
+
+        [
+            [206.8, 413.6, 413.6], # Durations, a. u. 
+            [0.00285, 0.01425, 0.01425], # Amplitudes, a. u.
+            [0.057, 0.114, 0.171], # Frequencies, a. u.
+            [0, 0, 0] # Phases, rad
         ]
-    ]
+    ])
