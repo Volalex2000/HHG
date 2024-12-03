5b349b81c0ced2e94eec8e60fd1a04911e0176e3
Volalex2000 <volkov.ad@phystech.edu>
2024-12-03 15:10:09 +0100

    1.5 version of Parameters.py

--- Parameters.py
+++ Parameters.py
@@ -2,35 +2,35 @@ def pars_YanPengPhysRevA_78_033821():
     return [
         [ 
             206.8, # Durations, a. u.
-            0.00997, # Amplitudes, a. u. 
+            0.1412, # Amplitudes of electric fields, a. u. 
             0.057, # Frequencies, a. u.
             0 # Phases
         ],
 
         [
             [206.8, 206.8, 206.8], # Durations, a. u. 
-            [0.00855, 0.00285, 0.00570], # Amplitudes, a. u.
+            [0.1308, 0.0755, 0.1068], # Amplitudes of electric fields, a. u.
             [0.057, 0.114, 0.171], # Frequencies, a. u.
             [0, 0, 0] # Phases, rad
         ],
 
         [
             [206.8, 206.8], # Durations, a. u.
-            [0.00912, 0.00313], # Amplitudes, a. u.
+            [0.1351, 0.07912], # Amplitudes of electric fields, a. u.
             [0.057, 0.114], # Frequencies, a. u.
             [0, 0] # Phases, rad
         ],
 
         [
             [165.4, 165.4, 165.4], # Durations, a. u. 
-            [0.00855, 0.00285, 0.00570], # Amplitudes, a. u.
+            [0.1308, 0.0755, 0.1068], # Amplitudes of electric fields, a. u.
             [0.057, 0.114, 0.171], # Frequencies, a. u.
             [0, 0, 0] # Phases, rad
         ],
 
         [
             [206.8, 413.6, 413.6], # Durations, a. u. 
-            [0.00285, 0.01425, 0.01425], # Amplitudes, a. u.
+            [0.00285, 0.01425, 0.01425], # Amplitudes of electric fieldsa, a. u.
             [0.057, 0.114, 0.171], # Frequencies, a. u.
             [0, 0, 0] # Phases, rad
         ]
